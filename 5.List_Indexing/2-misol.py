a3e57a59978e0715c365ebc61aebe8ee38e781a1
Abdurasull <abdurasulsalimov94@gmail.com>
2025-01-01 00:00:03 +0500

    bajarildi

--- 5.List_Indexing/2-misol.py
+++ 5.List_Indexing/2-misol.py
@@ -1,4 +1,4 @@
 #Bu erda hohlasak list hohlasak esa tuple dan foydalanishimiz mumkin chunki shartimizda ruyxat elementlarini o`zgartirish haqida hichnima diyilmagan
 tuple_1 = "Akmal", "Bobur", "Muhammadjon", "AbduKarim", "Sobir"
 
-print("result: ", tuple_1[1], tuple_1[3])
+print("result: ", tuple_1[len(tuple_1) - 1])
---
+++ 5.List_Indexing/3-misol.py
@@ -0,0 +1,4 @@
+#Bu erda hohlasak list hohlasak esa tuple dan foydalanishimiz mumkin chunki shartimizda ruyxat elementlarini o`zgartirish haqida hichnima diyilmagan
+tuple_1 = 21, "Akmal", "Bobur", "Muhammadjon", "AbduKarim", "Sobir", True
+
+print("result: ", tuple_1[0],tuple_1[len(tuple_1) - 1], tuple_1[int(len(tuple_1)/2)])
