3510cc44d6b072a8b76fd604136ac0006b420d81
Abdurasull <abdurasulsalimov94@gmail.com>
2025-01-01 12:06:17 +0500

    bajarildi

---
+++ 10.Tuple_Slicing/1-misol.py
@@ -0,0 +1,3 @@
+tuple_1 = 12, 55.6, "Qish fasli", True, False, "Baxor"
+
+print("result: {}, {}, {}".format(tuple_1[0], tuple_1[1], tuple_1[2]))
---
+++ 10.Tuple_Slicing/3-misol.py
@@ -0,0 +1,5 @@
+tuple_1 = 12, 55.6, "Qish fasli", True, False, 6, 8, 99, 55, 10,"Assalomu alaykum", 5555
+
+for i in range(12):
+    if i % 2 == 1:
+        print(tuple_1[i], end=" ")
