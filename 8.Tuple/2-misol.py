bb62f16c42065f122ab9f630ec82302df1f65184
Abdurasull <abdurasulsalimov94@gmail.com>
2025-01-01 09:13:49 +0500

    bajarildi

---
+++ 8.Tuple/2-misol.py
@@ -0,0 +1,5 @@
+tuple_1 = 21, 12, 31, 28
+#tuple immutable ya`ni o`zgarmas bo`lgani uchun undagi elementlarni o`zgartira olmaymiz va unga hichqanday yangi element qo`sha olmaymiz`
+tuple_1.append(333)#Bu yerda bizga xato chop etadi chunki biz tuple ga yangi element qo`shmoqchi bo`lyabmiz
+
+print(tuple_1)
