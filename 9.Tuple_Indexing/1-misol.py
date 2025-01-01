0edbf5d0bc96fbf567d46d94063754da649d3f06
Abdurasull <abdurasulsalimov94@gmail.com>
2025-01-01 11:55:30 +0500

    bajarildi

---
+++ 9.Tuple_Indexing/1-misol.py
@@ -0,0 +1,3 @@
+tuple_1 = 12, 55.6, "Qish fasli", True, False
+
+print("result: {}".format(tuple_1[2]))
---
+++ 9.Tuple_Indexing/3-misol.py
@@ -0,0 +1,7 @@
+tuple_1 = 12, 55.6, "Qish fasli", True, False, 6, 8, 99, 55, 10
+
+# 2- va 8- elementlarni chop etamiz
+print("1-element:{}, 2-element: {}".format(tuple_1[1], tuple_1[7]))
+
+# 2- va 8- elementlar yigindisi
+print("Result: {}".format(tuple_1[1] + tuple_1[7]))
